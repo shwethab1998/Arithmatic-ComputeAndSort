d6a41accceeeff63a685ac13d4720b146838eaca
“Shwetha <shwethabalaji87@gmail.com>
2020-11-02 22:22:56 +0530

    Taking Arithmatic Values as Input

---
+++ ArithInput.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Enter any 3 Arithmatic Values as Input....."
+
+read -p "Enter any num : " a
+read -p "Enter any num : " b
+read -p "Enter any num : " c
+
+echo "Inputs are:"
+echo $a
+echo $b
+echo $c
